bc2a23d95794f89009cfdbfe4a4c94811d92e6d3
akhil <akhilr37@gmail.com>
2022-03-28 08:41:59 +0000

    interface design completed

--- src/components/InputComponent/index.js
+++ src/components/InputComponent/index.js
@@ -1,9 +1,82 @@
 import {Component} from 'react'
 import './index.css'
+import {v4} from 'uuid'
 import PasswordItems from '../PasswordItems'
 
 class InputComponent extends Component {
+  state = {
+    details: [],
+    Website: '',
+    Name: '',
+    Password: '',
+    hidden: true,
+  }
+
+  onWebsiteInput = event => {
+    this.setState({Website: event.target.value})
+  }
+
+  onNameInput = event => {
+    this.setState({Name: event.target.value})
+  }
+
+  onPasswordInput = event => {
+    this.setState({Password: event.target.value})
+  }
+
+  onClickingAdd = () => {
+    const {Name, Website, Password, details} = this.state
+    const creds = {id: v4(), name: Name, website: Website, password: Password}
+    this.setState(prevState => ({
+      details: [...prevState.details, creds],
+      Website: '',
+      Name: '',
+      Password: '',
+    }))
+  }
+
+  checkingBox = () => {
+    this.setState(prevState => ({hidden: !prevState.hidden}))
+  }
+
+  onDeleteClick = id => {
+    const {details} = this.state
+    this.setState({details: details.filter(vals => vals.id !== id)})
+  }
+
   render() {
+    const {details, Name, Website, Password, hidden} = this.state
+    console.log(hidden)
+    const totalPasswords = details.length
+    const g = details.length === 0
+    const noPasswordSite = (
+      <ul className="passwordItemsContainer">
+        <img
+          alt="no passwords"
+          className="noPassImg"
+          src="https://assets.ccbp.in/frontend/react-js/no-passwords-img.png"
+        />
+        <p className="noText">No passwords</p>
+      </ul>
+    )
+    const hasPassword = (
+      <ul className="passwordItemsContainer">
+        {details.map(item => (
+          <PasswordItems
+            id={item.id}
+            key={item.id}
+            website={item.website}
+            name={item.name}
+            password={item.password}
+            hidden={hidden}
+            onDeleteClick={this.onDeleteClick}
+          />
+        ))}
+      </ul>
+    )
+
+    const rendering = g ? noPasswordSite : hasPassword
+
     return (
       <div className="totalContainer">
         <div className="contentContainer">
@@ -31,6 +104,8 @@ class InputComponent extends Component {
                     className="websiteInput"
                     placeholder="Enter Website"
                     type="text"
+                    onChange={this.onWebsiteInput}
+                    value={Website}
                   />
                 </div>
                 <div className="inputELandLogoContainer">
@@ -43,6 +118,8 @@ class InputComponent extends Component {
                     type="text"
                     className="websiteInput"
                     placeholder="Enter Username"
+                    onChange={this.onNameInput}
+                    value={Name}
                   />
                 </div>
                 <div className="inputELandLogoContainer">
@@ -55,9 +132,15 @@ class InputComponent extends Component {
                     type="password"
                     className="websiteInput"
                     placeholder="Enter Password"
+                    onChange={this.onPasswordInput}
+                    value={Password}
                   />
                 </div>
-                <button className="buttonCss" type="submit">
+                <button
+                  className="buttonCss"
+                  type="submit"
+                  onClick={this.onClickingAdd}
+                >
                   Add
                 </button>
               </div>
@@ -67,7 +150,7 @@ class InputComponent extends Component {
             <div className="countandInputContainer">
               <div className="countContainer">
                 <p className="passwordsText">Your Passwords</p>
-                <p className="count">1</p>
+                <p className="count">{totalPasswords}</p>
               </div>
               <div className="searchContainer">
                 <img
@@ -85,18 +168,17 @@ class InputComponent extends Component {
             <hr className="linecss" />
             <div className="belowContainer">
               <div className="showPasswordsContainer">
-                <input id="showpass" type="checkbox" />
+                <input
+                  id="showpass"
+                  type="checkbox"
+                  onChange={this.checkingBox}
+                />
                 <label htmlFor="showpass" className="checkText">
                   Show Passwords
                 </label>
               </div>
-              <ul className="passwordItemsContainer">
-                <PasswordItems
-                  website="www.akhil.com"
-                  name="akhil"
-                  password="qwerty"
-                />
-              </ul>
+
+              {rendering}
             </div>
           </div>
         </div>
--- src/components/PasswordItems/index.js
+++ src/components/PasswordItems/index.js
@@ -1,11 +1,43 @@
 import './index.css'
 
 const PasswordItems = props => {
-  const {website, name, pasword} = props
+  const {id, website, name, password, hidden, onDeleteClick} = props
   const initial = name[0]
+  const BGcolors = [
+    'initialBg0',
+    'initialBg1',
+    'initialBg2',
+    'initialBg3',
+    'initialBg4',
+    'initialBg5',
+  ]
+  const ondeleteAction = () => {
+    onDeleteClick(id)
+  }
+  const bgColor = BGcolors[Math.ceil(Math.random() * BGcolors.length - 1)]
+
   return (
     <li className="passwordsContainer">
-      <p className=>{initial.toUpperCase()}</p>
+      <p className={`initialCss ${bgColor}`}>{initial.toUpperCase()}</p>
+      <div className="credentialsContainer">
+        <p className="details website">{website}</p>
+        <p className="details name">{name}</p>
+        <p className="details password">
+          {password}
+          {/* <img
+            className="starsCss"
+            alt="stars"
+            src="https://assets.ccbp.in/frontend/react-js/password-manager-stars-img.png"
+          /> */}
+        </p>
+      </div>
+      <button className="delButton" onClick={ondeleteAction}>
+        <img
+          className="delImgCss"
+          alt="delete"
+          src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
+        />
+      </button>
     </li>
   )
 }
